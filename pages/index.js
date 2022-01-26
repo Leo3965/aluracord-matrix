eacf204164e3e3f4b0674e47a3f6820879673dd9
Léo Freitas <leonardo.freitas@certisign.com.br>
2022-01-26 19:29:41 -0300

    add gibhub friends component

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import {React, useState} from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -60,110 +33,311 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'leo3965';
+  // const username = 'omariosouto';
+  const [username, setUsername] = useState('leo3965');
+  const [internalUsername, setInternalUsername] = useState('leo396');
+  const [follower1, setFollower1] = useState('')
+  const [follower2, setFollower2] = useState('')
+  const [follower3, setFollower3] = useState('')
+  const router = useRouter();
+
+  function getFollowers(user) {
+    let followers = [];
+      fetch(`https://api.github.com/users/${user}/followers`, {
+        method: 'GET'
+      })
+      .then((response) => {
+        if (response.status == 200) return response.json();
+      })
+      .then((response) => {
+        setFollower1(response[0].login);
+        setFollower2(response[1].login);
+        setFollower3(response[2].login);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+    
+      
+  }
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          //backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          //backgroundImage: 'url(../imgs/billy-huynh-W8KTS-mhFUE-unsplash.jpg)',
+          backgroundImage: 'url(https://images.unsplash.com/photo-1496715976403-7e36dc43f17b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
-        <Box
-          styleSheet={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            flexDirection: {
-              xs: 'column',
-              sm: 'row',
-            },
-            width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
-            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[700],
-          }}
-        >
-          {/* Formulário */}
-          <Box
-            as="form"
-            styleSheet={{
-              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
-            }}
-          >
-            <Titulo tag="h2">Boas vindas de volta!</Titulo>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
-              {appConfig.name}
-            </Text>
-
-            <TextField
-              fullWidth
-              textFieldColors={{
-                neutral: {
-                  textColor: appConfig.theme.colors.neutrals[200],
-                  mainColor: appConfig.theme.colors.neutrals[900],
-                  mainColorHighlight: appConfig.theme.colors.primary[500],
-                  backgroundColor: appConfig.theme.colors.neutrals[800],
-                },
-              }}
-            />
-            <Button
-              type='submit'
-              label='Entrar'
-              fullWidth
-              buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.primary[500],
-                mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
-              }}
-            />
-          </Box>
-          {/* Formulário */}
-
-
-          {/* Photo Area */}
+        <Box styleSheet= {{
+          display: 'box'
+        }}>
           <Box
             styleSheet={{
               display: 'flex',
-              flexDirection: 'column',
               alignItems: 'center',
-              maxWidth: '200px',
-              padding: '16px',
-              backgroundColor: appConfig.theme.colors.neutrals[800],
-              border: '1px solid',
-              borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: '10px',
-              flex: 1,
-              minHeight: '240px',
+              justifyContent: 'space-between',
+              flexDirection: {
+                xs: 'column',
+                sm: 'row',
+              },
+              width: '100%', maxWidth: '700px',
+              borderRadius: '5px', padding: '32px', margin: '16px',
+              boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+              backgroundColor: appConfig.theme.colors.neutrals[700],
             }}
           >
-            <Image
-              styleSheet={{
-                borderRadius: '50%',
-                marginBottom: '16px',
-                border: `1px solid ${appConfig.theme.colors.primary[300]}`
+            {/* Formulário */}
+            <Box
+              as="form"
+              onSubmit={function (event) {
+                event.preventDefault();
+                console.log('Alguém submeteu o form');
+                router.push('/chat');
+                // window.location.href = '/chat';
               }}
-              src={`https://github.com/${username}.png`}
-            />
-            <Text
-              variant="body4"
               styleSheet={{
-                color: appConfig.theme.colors.neutrals[200],
-                backgroundColor: appConfig.theme.colors.neutrals[900],
-                padding: '3px 10px',
-                borderRadius: '1000px'
+                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
               }}
             >
-              {username}
-            </Text>
+              <Titulo tag="h2">Boas vindas de volta!</Titulo>
+              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+                {appConfig.name}
+              </Text>
+
+              {/* <input
+                              type="text"
+                              value={username}
+                              onChange={function (event) {
+                                  console.log('usuario digitou', event.target.value);
+                                  // Onde ta o valor?
+                                  const valor = event.target.value;
+                                  // Trocar o valor da variavel
+                                  // através do React e avise quem precisa
+                                  setUsername(valor);
+                              }}
+                          /> */}
+              <TextField
+                value={internalUsername}
+                onChange={function (event) {
+                  // Onde ta o valor?
+                  let input = event.target.value;
+                  setInternalUsername(input);
+                  //console.log('usuario digitou', a);
+                  //console.log('internal', username);
+
+                  if (input.length >= 3) {
+                    // Trocar o valor da variavel
+                    // através do React e avise quem precisa
+                    getFollowers(input);
+                    setUsername(input);
+                  }
+                }}
+                fullWidth
+                textFieldColors={{
+                  neutral: {
+                    textColor: appConfig.theme.colors.neutrals[200],
+                    mainColor: appConfig.theme.colors.neutrals[900],
+                    mainColorHighlight: appConfig.theme.colors.primary[500],
+                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                  },
+                }}
+              />
+              <Button
+                type='submit'
+                label='Entrar'
+                fullWidth
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["000"],
+                  mainColor: appConfig.theme.colors.primary[500],
+                  mainColorLight: appConfig.theme.colors.primary[400],
+                  mainColorStrong: appConfig.theme.colors.primary[600],
+                }}
+              />
+            </Box>
+            {/* Formulário */}
+
+
+            {/* Photo Area */}
+            <Box
+              styleSheet={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                maxWidth: '200px',
+                padding: '16px',
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                border: '1px solid',
+                borderColor: appConfig.theme.colors.neutrals[999],
+                borderRadius: '10px',
+                flex: 1,
+                minHeight: '240px',
+              }}
+            >
+              <Image
+                styleSheet={{
+                  borderRadius: '50%',
+                  marginBottom: '16px',
+                }}
+                src={`https://github.com/${username}.png`}
+              />
+              <Text
+                variant="body4"
+                styleSheet={{
+                  color: appConfig.theme.colors.neutrals[200],
+                  backgroundColor: appConfig.theme.colors.neutrals[900],
+                  padding: '3px 10px',
+                  borderRadius: '1000px'
+                }}
+              >
+                {username}
+              </Text>
+            </Box>
+            {/* Photo Area */}
           </Box>
-          {/* Photo Area */}
+
+          {/* Followers Area */}
+          <Box styleSheet= {{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: {
+              xs: 'column',
+              sm: 'column',
+            },
+            width: '100%', maxWidth: '700px',
+            borderRadius: '5px', padding: '12px', margin: '16px',
+            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+            backgroundColor: appConfig.theme.colors.neutrals[700],
+            gap: '12px'
+          }}>
+          <Titulo tag="h2">GitHub Friends!</Titulo>
+            <Box styleSheet={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-around',
+                flexDirection: {
+                  xs: 'column',
+                  sm: 'row',
+                },
+                width: '100%', maxWidth: '700px'
+              }}>
+              
+              
+              <Box
+                styleSheet={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  maxWidth: '70px',
+                  padding: '4px',
+                  backgroundColor: appConfig.theme.colors.neutrals[800],
+                  border: '1px solid',
+                  borderColor: appConfig.theme.colors.neutrals[999],
+                  borderRadius: '10px',
+                  flex: 1,
+                  minHeight: '80px',
+                }}
+              >
+                <Image
+                  styleSheet={{
+                    borderRadius: '50%',
+                    marginBottom: '16px',
+                  }}
+                  src={`https://github.com/${follower1 || 'github'}.png`}
+                />
+                <Text
+                  variant="body4"
+                  styleSheet={{
+                    color: appConfig.theme.colors.neutrals[200],
+                    backgroundColor: appConfig.theme.colors.neutrals[900],
+                    padding: '3px 10px',
+                    borderRadius: '1000px'
+                  }}
+                >
+                  {follower1 || 'github'}
+                </Text>
+              </Box>
+              {/* Follower 1*/}
+
+              <Box
+                styleSheet={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  maxWidth: '70px',
+                  padding: '4px',
+                  backgroundColor: appConfig.theme.colors.neutrals[800],
+                  border: '1px solid',
+                  borderColor: appConfig.theme.colors.neutrals[999],
+                  borderRadius: '10px',
+                  flex: 1,
+                  minHeight: '80px',
+                }}
+              >
+                <Image
+                  styleSheet={{
+                    borderRadius: '50%',
+                    marginBottom: '16px',
+                  }}
+                  src={`https://github.com/${follower2 || 'github'}.png`}
+                />
+                <Text
+                  variant="body4"
+                  styleSheet={{
+                    color: appConfig.theme.colors.neutrals[200],
+                    backgroundColor: appConfig.theme.colors.neutrals[900],
+                    padding: '3px 10px',
+                    borderRadius: '1000px'
+                  }}
+                >
+                  {follower2 || 'github'}
+                </Text>
+              </Box>
+              {/* Follower 2*/}
+
+              <Box
+                styleSheet={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  maxWidth: '70px',
+                  padding: '4px',
+                  backgroundColor: appConfig.theme.colors.neutrals[800],
+                  border: '1px solid',
+                  borderColor: appConfig.theme.colors.neutrals[999],
+                  borderRadius: '10px',
+                  flex: 1,
+                  minHeight: '80px',
+                }}
+              >
+                <Image
+                  styleSheet={{
+                    borderRadius: '50%',
+                    marginBottom: '16px',
+                  }}
+                  src={`https://github.com/${follower3 || 'github'}.png`}
+                />
+                <Text
+                  variant="body4"
+                  styleSheet={{
+                    color: appConfig.theme.colors.neutrals[200],
+                    backgroundColor: appConfig.theme.colors.neutrals[900],
+                    padding: '3px 10px',
+                    borderRadius: '1000px'
+                  }}
+                >
+                  {follower3 || 'github'}
+                </Text>
+              </Box>
+              {/* Follower 3*/}
+
+            </Box>
+          </Box>
         </Box>
       </Box>
     </>
